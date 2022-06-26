655e95d8ac5278c07c79be7050b2c2ddeb85d9eb
memokey <skytopground@gmail.com>
2022-06-26 04:17:18 +0200

    done mobile touch

--- src/components/ChatPrivateModel/OwnThird/index.js
+++ src/components/ChatPrivateModel/OwnThird/index.js
@@ -39,7 +39,7 @@ const OwnThird = ({ roomInfo, modelURL, name }) => {
           <a-entity 
             id="rig"
             position="0 1.65 0" 
-            look-controls="pointerLockEnabled: true; reverseMouseDrag: false"
+            look-controls="pointerLockEnabled: true; reverseMouseDrag: true"
             movement-controls="speed: 0.2"
             simple-navmesh-constraint="navmesh:#navmesh;fall: 5;height:1.65;" 
             networked="template:#avatar-template;attachTemplateToLocal:false;"
