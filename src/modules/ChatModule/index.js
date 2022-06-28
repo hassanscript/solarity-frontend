9b98a6f18bafc86e06c2d9059ccd2ad191838c25
memokey <skytopground@gmail.com>
2022-06-28 05:38:47 +0200

    add loading screen

--- src/modules/ChatModule/index.js
+++ src/modules/ChatModule/index.js
@@ -285,17 +285,28 @@ const ChatModule = () => {
   if (mounted && models && models[modelIndex] && models[modelIndex].modelUrl) {
     return (
       <div>
-        <video className={styles.background_video} id="background_video" autoPlay loop muted>
-          {!checkBrowser() && (<source src="/assets/video/loading_video.mp4" type="video/mp4" />)}
-        </video>
-        <div id="loadingScreen" className={styles.loadingScreen}>
-          <div id="loadingText" className={styles.loadingText}>
-          </div>
-          <div id="loadingBar" className={styles.loadingBar}>
-          </div>
-          <div id="loading_label" className={styles.loading_label}>
-            POWERED BY SOLARITY
-            <img id="loading_logo" className={styles.loading_logo} src="/assets/images/loading_logo.png" alt="loadig_logo" />
+        <div id="loadingScreen" className="fixed top-0 left-0 right-0 bottom-0">
+          <div className="h-full w-full bg-[rgba(12,12,14,0.7)] backdrop-blur-lg pt-[calc(50vh-104px)] sm:pt-[calc(50vh-165px)]">
+            <div className="w-[210px] h-[210px] sm:w-[330px] sm:h-[330px] m-auto">
+              <div className="text-white items-center flex h-full">
+                <div className="text-center m-auto h-full w-full">
+                  <div className="progress relative h-full w-full">
+                    <svg className="circle-loading-bar hidden sm:block w-full h-full">
+                      <circle cx="165" cy="165" r="160"></circle>
+                      <circle cx="165" cy="165" r="160" style={{"--percent": 0}}></circle>
+                    </svg>
+                    <svg className="circle-loading-bar block sm:hidden w-full h-full">
+                      <circle cx="104" cy="104" r="100"></circle>
+                      <circle cx="104" cy="104" r="100" style={{"--percent": 0}}></circle>
+                    </svg>
+                    <div className="absolute left-[65px] top-[60px] sm:top-[90px] sm:left-[105px]">
+                      <h2 className="loading-status text-[40px] sm:text-[70px] font-bold font-['Outfit'] mb-2 sm:mb-5">0</h2>
+                      <span className="text-xs sm:text-lg">loading models</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div id="sceneWrapper" style={{ opacity: "0" }}>
