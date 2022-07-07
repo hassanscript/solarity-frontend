969c4e0f3bbb7eecd2fe912dba69f9c4114b1b73
memokey <skytopground@gmail.com>
2022-07-07 23:44:21 +0200

    changed toolbar position in extension

--- src/components/ChatToolbar/index.tsx
+++ src/components/ChatToolbar/index.tsx
@@ -32,7 +32,7 @@ const ChatToolbar: FC<ChatToolbarProps> = ({
 }) => {
 
   return (
-    <div className='fixed flex bottom-[5vh] left-[10px] sm:left-[30px] rounded-lg bg-brandblack px-4 py-2'>
+    <div className='fixed flex top-[5vh] sm:bottom-[5vh] left-[10px] sm:left-[30px] rounded-lg bg-brandblack px-4 py-2'>
         <div className='p-2 border border-gray-600 rounded-lg mx-1 cursor-pointer hover:border-gray-400' onClick={() => handelMuteBtnClick()}>
             <audio
             id="player-audio"
