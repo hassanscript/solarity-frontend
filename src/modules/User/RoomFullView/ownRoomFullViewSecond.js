b48f944a6657aae0d7494a35d8f6cb0727d4b2f5
memokey <skytopground@gmail.com>
2022-07-09 00:22:21 +0200

    changed NFT view

--- src/modules/User/RoomFullView/ownRoomFullViewSecond.js
+++ src/modules/User/RoomFullView/ownRoomFullViewSecond.js
@@ -220,15 +220,15 @@ export default function RoomFullView({user}) {
               <Close />
             </div>
           </div>
-          <div className="grid grid-cols-5 sm:grid-cols-4 mb-4">
+          <div className="grid grid-cols-3 sm:grid-cols-4 mb-4">
             <div className="col-span-3 mx-5 p-5 pr-2 border-[2px] rounded-l-xl border-secondary">
-              <div className='w-full h-[calc(100vh-148px)] overflow-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 pr-3'>
+              <div className='w-full h-[42vw] sm:h-[calc(100vh-148px)] overflow-auto grid grid-cols-3 sm:grid-cols-2 md:grid-cols-3 gap-3 pr-3'>
                 {nfts && nfts.map((data, index) => (
-                  <NftCard key={"nftCard-" + index} selected={index == selectedCardIndex} {...data} onClick={() => selectCard(index)} />
+                  <NftCard key={"nftCard-" + index} selected={index == selectedCardIndex} mini={getWidth() < 640 ? true: false} {...data} onClick={() => selectCard(index)} />
                 ))}
               </div>
             </div>
-            <div className='col-span-2 sm:col-span-1 border-[2px] rounded-r-xl border-secondary mr-5 p-5'>
+            <div className='hidden sm:block col-span-2 sm:col-span-1 border-[2px] rounded-r-xl border-secondary mr-5 p-5'>
               {nfts[selectedCardIndex] && (
                 <div>
                   <h3 className='text-lg sm:text-xl mb-5'>{nfts[selectedCardIndex].name}</h3>
